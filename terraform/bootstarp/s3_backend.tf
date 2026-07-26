c573ef00d3c819668e8875576d060bc0c88bed47
Sandip <konjno29@.com>
2026-07-26 12:01:23 +0000

    feat(bootstrap): create remote backend using S3 and DynamoDB

---
+++ terraform/bootstarp/s3_backend.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "cloud-projects-terraform-state-2026"
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "state_bucket" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "state_locking"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
