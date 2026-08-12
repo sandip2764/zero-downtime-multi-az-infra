b7bd3f7fb88854270e2b025befc3cc5028b0f30c
Sandip <konjno29@.com>
2026-08-12 15:34:24 +0000

    fix: modify enviroment ( backend.tf configuration )

--- terraform/environment/backend.tf
+++ terraform/environment/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "cloud-projects-terraform-state-2026"
-    key          = "multi-az/terraform.tfstate"
+    key          = "environment/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
   }
